e4a8e7c0c634f32a79f4c5f2c9d5047e564ab6a9
WojtekPoplawski <wojtek.poplawski@hotmail.com>
2022-01-25 09:19:14 +0100

    added outputTable

--- src/Modnisia.java
+++ src/Modnisia.java
@@ -6,6 +6,7 @@ public class Modnisia {
     int i;
     int W;
     int[][] inputTable;
+    int[][] outputTable;
     public Modnisia() {
 
     }
@@ -19,6 +20,9 @@ public class Modnisia {
             inputTable[j][0]=in.nextInt();
             inputTable[j][1]=in.nextInt();
         }
+        this.outputTable = new int[W+1][i+1];
+        for(int x=0;x<=W;x++) this.outputTable[x][0]=0;
+        for(int x=0;x<=i;x++) this.outputTable[0][i]=0;
     }
     void out(){
 
